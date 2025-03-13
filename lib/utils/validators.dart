21236f53fb1156622bc478f2ce364ef239424784
thanushka <thanushkapraveen@ymail.com>
2025-03-14 02:23:36 +1300

    add: validation condition changes pw field

--- lib/utils/validators.dart
+++ lib/utils/validators.dart
@@ -17,8 +17,8 @@ class Validators {
   static String? validatePassword(String? value) {
     if (value == null || value.isEmpty) {
       return 'Password cannot be empty';
-    } else if (value.length < 6) {
-      return 'Password must be at least 6 characters long';
+    } else if (value.length < 5) {
+      return 'Password must be at least 5 characters long';
     }
     return null;
   }
